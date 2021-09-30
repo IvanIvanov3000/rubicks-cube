eec9fa0018f51b98a5010f4e2c6c759277696a77
Ivan <ivan_ivanov_3000@abv.bg>
2021-09-30 18:41:23 +0300

    Express started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("start");
+const express = require('express');
+const app = express();
+
+app.all("/", function (req, res) {
+    res.write('Hello');
+    res.end();
+});
+app.listen(3000, () => console.log('Running on port 3000'))
