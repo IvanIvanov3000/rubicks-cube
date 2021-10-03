fa6c613a8124d40f92dba1f198188c01a9391dc1
Ivan <ivan_ivanov_3000@abv.bg>
2021-10-03 09:44:50 +0300

    Add about page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,7 +4,7 @@ const cubeService = require('../services/cubeService');
 const router = express.Router();
 
 const home = (req, res) => {
-    let cubes = cubeService.getAll()
+    let cubes = cubeService.getAll();
 
     res.render("index", {
         cubes: cubes
@@ -12,5 +12,11 @@ const home = (req, res) => {
 
 }
 
+const about = (req, res) => {
+    res.render("about");
+}
+
 router.get("/", home);
+router.get("/about", about);
+
 module.exports = router;
