924f793eb57b41142312df0891f41a6ca8921a98
Ivan <ivan_ivanov_3000@abv.bg>
2021-10-07 17:01:23 +0300

    Add accessory page with form

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -5,5 +5,11 @@ const router = express.Router();
 router.get('/create', (req, res) => {
 
     res.render('accessory/create')
-})
+});
+
+router.post('/create', (req, res) => {
+    let accessory = req.body;
+    console.log(accessory);
+    res.redirect("/");
+});
 module.exports = router;
