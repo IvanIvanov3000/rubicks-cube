feb396da3dc26b7b1e98a64c47a5187ec79806b0
Ivan <ivan_ivanov_3000@abv.bg>
2021-10-16 13:40:31 +0300

    Login and register links

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,18 @@ const express = require('express');
 const cubeController = require("./controllers/cubeController");
 const homeController = require("./controllers/homeController");
 const accessoryController = require("./controllers/accessoryController");
-
+const authController = require("./controllers/authController");
 
 const router = express.Router();
 
 router.use("/cube", cubeController);
 router.use(homeController);
 router.use("/accessory", accessoryController);
+router.use("/authorization", authController);
 //Error
 router.use("*", (req, res) => {
-    res.render("404");
+    res.status(404).render("404");
 });
+
 
 module.exports = router;
