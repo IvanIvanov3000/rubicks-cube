cdd0f7f04180f2c766ff13c5c4e10e2dc9f72053
Ivan <ivan_ivanov_3000@abv.bg>
2021-10-17 16:38:18 +0300

    Update model User

--- src/models/User.js
+++ src/models/User.js
@@ -3,36 +3,39 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {
         type: 'string',
+        unique: true,
+        vallidate : [/^[a-zA-Z0-9]+$/i, "Username should contain only endlish letters and numbers!"],
         required: true,
-        minLength: [2, "Username cannot be less than 2 symbols."]
+        minLength: [5, "Username cannot be less than 5 symbols."]
     },
     password: {
+        vallidate : [/^[a-zA-Z0-9]+$/i, "Password should contain only endlish letters and numbers!"],
         type: 'string',
-        required: true
+        required: true,
+        minLength: [8, "Password cannot be less than 8 symbols."]
+
     }
 
 });
-userSchema.pre('save', function(next) {
+userSchema.pre('save', function (next) {
     bcrypt.hash(this.password, 10)
         .then(hash => {
             this.password = hash;
-            
+
             next();
         });
 });
 
-userSchema.static('findByUsername', function(username) {
-    return this.findOne({username});
+userSchema.static('findByUsername', function (username) {
+    return this.findOne({ username });
 });
-
-userSchema.method('validatePassword', function(password) {
+userSchema.method('validatePassword', function (password) {
     return bcrypt.compare(password, this.password);
 });
-
 userSchema.virtual('repeatPassword')
-    .set(function(v) {
+    .set(function (v) {
         if (v !== this.password) {
-            throw new Error('Password MIssmatch');
+            throw new Error('Password Missmatch');
         }
     });
 
