eef6433977f2f2dcb4c52a792b881e72c9eaac12
Ivan <ivan_ivanov_3000@abv.bg>
2021-10-24 11:13:18 +0300

    Update Error handler middleware

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,6 +1,6 @@
 function errorHandler(error, req, res, next) {
     if (error) {
-        res.locals.error = error;
+        res.locals.errors = [error];
         res.status(404).render("404");
     }
 }
